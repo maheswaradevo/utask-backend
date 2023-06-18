0011a87f38a2760cc104c2deb7ebaf4e643f9174
maheswaradevo <pundadevo21@gmail.com>
2023-06-18 15:35:50 +0800

    fix: change port

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -19,7 +19,7 @@ var (
 	OauthConfGl = &oauth2.Config{
 		ClientID:     "",
 		ClientSecret: "",
-		RedirectURL:  "http://localhost:3000/auth/google/callback",
+		RedirectURL:  "http://0.0.0.0:3000/auth/google/callback",
 		Scopes:       []string{"https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/calendar.events", "https://www.googleapis.com/auth/calendar"},
 		Endpoint:     google.Endpoint,
 	}
