1e2dc758ce4c68c078d223b53ad0cb1d6ca62045
maheswaradevo <pundadevo21@gmail.com>
2023-06-19 16:54:34 +0800

    fix: add duration in the token

--- pkg/common/helpers/common.go
+++ pkg/common/helpers/common.go
@@ -25,6 +25,7 @@ func HandleLogin(w http.ResponseWriter, r *http.Request, oauthConf *oauth2.Confi
 	parameters.Add("state", oauthStateString)
 	parameters.Add("access_type", "offline")
 	parameters.Add("prompt", "consent")
+	parameters.Add("expires_in", "28800")
 	URL.RawQuery = parameters.Encode()
 	url := URL.String()
 	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
